4abdb6654d527eed4e6826231335f0a601324799
Babaganoush07 <94538153+Babaganoush07@users.noreply.github.com>
2022-04-07 05:23:33 -0700

    Update python_principals.py

--- python_principals.py
+++ python_principals.py
@@ -71,3 +71,32 @@ def random_number():
     return random.randint(1,100)
     
 print(random_number())
+
+'''
+Type check
+Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+
+For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+'''
+def only_ints(a,b):
+    return type(a) == int and type(b) == int
+ 
+print(only_ints(1,'a'))
+
+'''
+Double letters
+The goal of this challenge is to analyze a string to check if it contains two of the same letter in a row. 
+For example, the string "hello" has l twice in a row, while the string "nono" does not have two identical letters in a row.
+
+Define a function named double_letters that takes a single parameter. 
+The parameter is a string. Your function must return True if there are two identical letters in a row in the string, and False otherwise.
+'''
+def double_letters(string):
+    for i in range(len(string) - 1):
+        letter1 = string[i]
+        letter2 = string[i+1]
+        if letter1 == letter2:
+            return True
+    return False
+
+print(double_letters("hello"))
